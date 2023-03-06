d1282e4c4ce6ebc56cafdd25bc2733dbbe9caf61
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-03-06 11:15:56 +0200

    Update OmaMoodul.py

--- OmaMoodul.py
+++ OmaMoodul.py
@@ -15,3 +15,9 @@ def Kirjuta_failisse(fail:str,jarjend:list):
     for line in jarjend: #Перебираем строчки в списке
         f.write(line+'\n') #Добавляем по строчно в список.
     f.close()
+
+from gtts import gTTS
+import os
+def Heli(text:str,keel:str):
+    obj=gTTS(text=text,lang=keel,slow=False).save("Heli.mp3")
+    os.system("heli.mp3")
