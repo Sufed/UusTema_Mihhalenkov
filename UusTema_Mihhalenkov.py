867b68e8bd1e621c49866b53f0ab41ae1337955c
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-03-06 10:52:30 +0200

    Update UusTema_Mihhalenkov.py

--- UusTema_Mihhalenkov.py
+++ UusTema_Mihhalenkov.py
@@ -1,1 +1,14 @@
+from OmaMoodul import *
+laused=[]
 
+while True:
+    print("")
+    v=int(input("1-Loeme failist\n2-Salvestame failisse\n3-Tekstist kõne\n"))
+    if v==1:
+        laused=Loe_failist("Laused.txt")
+        for line in laused:
+            print(line)
+    elif v==2:
+        line=input("Lisa lause: ")
+        laused.append(line)
+        Kirjuta_failisse("Laused.txt",laused)
